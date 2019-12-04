0263a086f85b67524247a459f08eed59b174aca0
Junior627 <junior62725@gmail.com>
2019-12-04 23:27:49 +0000

    RPS War

--- js/script.js
+++ js/script.js
@@ -1,11 +1,47 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
+$(document).ready(function() {
+  $("#war").click(function() {
+    let rChoice = $("#input").val();
+    let uChoice = rChoice.toLowerCase();
 
 
+    let comp = Math.random();
 
-// DOCUMENT READY FUNCTION BELOW
+    let cheese = ["rock","paper","scissors"];
 
+    let cheez = Math.floor(comp*cheese.length);
+
+    let enemy = cheese[cheez];
+    $("#stupid").append(`<li>${uChoice}</li>`);
+    $("#bloodshed").append(`<li>${enemy}</li>`);
+
+  $("input").val("");
+
+    if (uChoice === enemy){
+      alert("TIE");
+    }
+  else if (uChoice === "scissors" && enemy === "rock"){
+    alert("you lost :(");
+  }
+    else if (uChoice === "scissors" && enemy === "paper"){
+      alert("you won :)");
+    }
+      else if (uChoice === "paper" && enemy === "scissors"){
+    alert("you lost :(");
+  }
+    else if (uChoice === "paper" && enemy === "rock"){
+      alert("you won :)");
+    }
+      else if (uChoice === "rock" && enemy === "paper"){
+    alert("you lost :(");
+  }
+    else if (uChoice === "rock" && enemy === "scissors"){
+      alert("you won :)");
+    }
+    else{
+      alert("please put an actual choice next time :|")
+    }
+  });
+
+
+
+});
